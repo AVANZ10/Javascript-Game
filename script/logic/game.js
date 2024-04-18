450813bacb20d5eae607e5ebbfdfdd256c342ca4
AVANZ10 <151640975+AVANZ10@users.noreply.github.com>
2024-04-18 19:24:05 +0200

    Add files via upload

--- script/logic/game.js
+++ script/logic/game.js
@@ -2,34 +2,32 @@ import Player from "../models/player.js";
 import Sound from "../models/sound.js";
 import Sprite from "../models/sprite.js";
 import Hitbox from "../models/hitbox.js";
+import Enemy from "../models/enemy.js";
 
 class Game {
     playerNickname;
-    gigi;
 
     constructor(canvas, config, playerNickname) {
         this.config = config
         this.canvas = canvas;
         this.playerNickname = playerNickname;
         this.ctx = canvas.getContext('2d');
+        this.floor = 216;
     }
 
     init() {
-        
         this.canvas.style.position = 'absolute';
         this.canvas.width = this.config.BG_WIDTH;
         this.canvas.height = this.config.BG_HEIGHT;
         this.canvas.style.backgroundImage = "url('" + this.config.BACKGROUND_IMG_SRC + "')";
         this.canvas.style.backgroundSize = "contain";
-
-        this.ground = new Hitbox(0,40, this.canvas.width, 150);
-        this.player = new Player(this.config.PLAYER_SRC, this.playerNickname);
-        this.fireball = new Sprite(this.config.FIREBALL_SRC, 360, 360, 6, 1, 50, 50);
         this.obstacle = new Hitbox(550, 200, 100, 200);
+        this.ground = new Hitbox(0,40, this.canvas.width, 150);
+        this.player = new Player(this.config.PLAYER_SRC, this.playerNickname, this.enemy, this.obstacle);
+        this.fireball = new Sprite(this.config.FIREBALL_SRC, 360, 360, 6, 1, 50, 50);
+
         this.bgMusic = new Sound("assets/audio/background.mp3");
-        this.gigi = new Player(this.config.PLAYER_SRC, "Gigi");
-        this.gigi.position.x = this.canvas.width - 200; // Posiziona Gigi verso la fine della mappa
-        this.gigi.velocity.x = 0; // Assicurati che Gigi sia fermo
+
     }
 
     keyboardPressedHandler(key) {
@@ -65,51 +63,18 @@ class Game {
     }
 
     update() {
-        if(this.player.collision(this.ground)){
+        //console.log(this.player.position.y + " " + this.floor)
+        if(this.player.position.y < this.floor){
             if(this.player.velocity.y < 0){
                 this.player.velocity.y = 0;
                 this.player.canJump = true;
                 this.player.position.y = this.ground.position.y + this.player.height;
-                // console.log("Player pos: ", this.player.position);
-                // console.log("Ground: ", this.ground.position);
             }
             
         }
-        if (this.gigi.collision(this.ground)) {
-            if (this.gigi.velocity.y < 0) {
-                this.gigi.velocity.y = 0;
-                this.gigi.canJump = true;
-                this.gigi.position.y = this.ground.position.y + this.gigi.height;
-            }
-        }
-        if(this.player.collision(this.obstacle) || this.obstacle.collision(this.player)) {
-            if(this.player.position.y > this.obstacle.y + this.player.height){
-                this.player.velocity.y = 0;
-            }
-            /*
-            if(this.player.velocity.x > 0){
-                this.player.velocity.x = 0;
-            }
-            */
-            //this.player.position.x = this.obstacle.position.x - this.player.width;
-            console.log("PLAYER CONTRO IL MURO");
-        };
-
-        for (const f in this.player.bullets) {
-            if (!this.player.bullets[f].collisionHandled && this.player.bullets[f].collision(this.gigi)) {
-                this.player.bullets[f].collisionHandled = true; // Imposta il flag della collisione come gestito
-                this.gigi.hp304 -= 10;
-                console.log("Gigi HP: " + this.gigi.hp304);
-            }
-        }
-        
 
         this.player.update();
         this.fireball.update();
-        this.gigi.update();
-
-        
-        
     }
 
     playBgMusic() {
@@ -126,12 +91,11 @@ class Game {
         this.fireball.draw(this.ctx);
         this.ground.draw(this.ctx);
         this.obstacle.draw(this.ctx);
-        if (this.gigi.hp304 > 0) {
-            this.gigi.draw(this.ctx);
-        }
         
     }
 
 }
 
 export default Game;
+
+
--- script/models/player.js
+++ script/models/player.js
@@ -4,49 +4,53 @@ import conf from '../config.js';
 import Sprite from './sprite.js';
 import Fireball from './fireball.js';
 import Hitbox from './hitbox.js';
+import Enemy from './enemy.js';
 
 class Player extends Hitbox {
     name;
     score;
-    velocity; // e non speed, mi raccomando
+    velocity;
     hp304;
     currentImageIndex;
     images;
     moving;
     update_timer;
 
-    constructor(images_srcs, name) {
-        super(50, conf.GROUND_Y,165,175)
-
+    constructor(images_srcs, name, gigi, obstacle) {
+        super(50, conf.GROUND_Y, 165, 175)
+        this.obstacle = obstacle;
+        this.score = 0;
         this.name = name;
-        // importo le immagini dello sprite_sheet nel vettore di immagini
         this.images = [];
-        for(let src of images_srcs) {
+        for (let src of images_srcs) {
             let img = new Image();
             img.src = src;
             this.images.push(img);
         }
-        //  inizialmente uso la prima immagine
-        this.currentImageIndex = 0; 
+
+        this.currentImageIndex = 0;
 
         this.velocity = new Vector2D();
         this.velocity.set(0, 0);
-        this.hp304 = 104;
+        this.hp304 = 165;
         this.score = 0;
         this.moving = false;
         this.update_timer = new Clock(125);
         this.canJump = true;
         this.bullets = [];
+        this.enemies = [];
+        this.enemies.push(new Enemy(this.enemies, conf.PLAYER_SRC, "Gigi"));
+
+        setInterval(() => {
+            this.enemies.push(new Enemy(this.enemies, conf.PLAYER_SRC, "Gigi"));
+        }, 2500)
     }
 
     jump() {
-        if(this.canJump) {
-            this.velocity.y = 16;
-            //this.velocity.x = 10;
+        if (this.canJump) {
+            this.velocity.y = 32;
         }
-        //  TODO: Fix double jump based on whatever ground collision
-        // Not only ground
-        if(this.position.y > conf.GROUND_Y) {
+        if (this.position.y > conf.GROUND_Y) {
             this.canJump = false;
         }
     }
@@ -60,43 +64,117 @@ class Player extends Hitbox {
     }
 
     draw(ctx) {
-        ctx.drawImage(this.images[this.currentImageIndex], this.position.x, 
-            ctx.canvas.clientHeight - this.position.y, 
+        ctx.drawImage(this.images[this.currentImageIndex], this.position.x,
+            ctx.canvas.clientHeight - this.position.y,
             175, 175);
+        ctx.strokeStyle = "white";
+        ctx.strokeRect(this.position.x, (ctx.canvas.clientHeight - (this.position.y + 20)), 165, 20);
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.position.x, (ctx.canvas.clientHeight - (this.position.y + 20)), this.hp304, 20);
         ctx.font = "30px Verdana";
         ctx.fillStyle = "white";
-        ctx.fillText(this.name, this.position.x + 50, (ctx.canvas.clientHeight - (this.position.y + 5)));
+        ctx.fillText(this.name, this.position.x + 50, (ctx.canvas.clientHeight - (this.position.y + 25)));
         this.bullets.forEach((b) => b.draw(ctx));
-        ctx.fillStyle = "green";
-        ctx.fillRect(this.position.x, this.position.y - 30, this.hp304, 10);
-
+        this.enemies.forEach((e) => e.draw(ctx));
+        this.drawScore(ctx)
         super.draw(ctx);
-        
+
     }
 
     update() {
-        this.position.add(this.velocity);
-        this.moving = this.velocity.x != 0;
-        this.update_timer.update();
+        //algoritmo di collisione preso dal codice di Crema
+        const prevBottom = this.position.y;
+        const prevLeft = this.position.x;
+        const prevTop = this.position.y + this.height;
+        const prevRight = this.position.x + this.width;
 
-        //  accelerazione gravitazionale se lascio la terra
-        // if(this.position.y > conf.GROUND_Y) {
-        //     this.velocity.y -= 1.2;
-        // }
+        this.enemies.forEach((e) => {
+            this.bullets.forEach((b) => {
+                if (!e.deleted && b.collision(e)) {
+                    console.log("colliding")
+                    e.hp304 -= 20;
+                    this.score += 10;
+                    this.bullets.splice(this.bullets.indexOf(b), 1);
+                }
+            });
+        });
+
+        this.enemies.forEach((e) => {
+            if (this.x < e.position.x + e.width && this.x + this.width > e.position.x && this.y < e.position.y + e.height) {
+                window.alert("GAME OVER! \n Score: " + this.score + "\n Refresh the page to play again!");
+                this.reset();
+            }
+        });
+
+        this.position.add(this.velocity);
+        this.moving = this.velocity.x !== 0;
+        this.update_timer.update();
         this.velocity.y += -1.2;
-        if(this.moving) {
-            if(this.update_timer.tick()) {
+
+        const bottom = this.position.y;
+        const left = this.position.x;
+        const top = this.position.y + this.height;
+        const right = this.position.x + this.width;
+
+        const collision = this.obstacle;
+        console.log()
+
+        if (bottom < collision.position.y + collision.height && right > collision.position.x && top > collision.position.y && left < collision.position.x + collision.width) {
+            if (prevRight <= collision.position.x && right > collision.position.x) this.position.x = collision.position.x - this.width;
+            if (prevLeft >= collision.position.x + collision.width && left < collision.position.x + collision.width) this.position.x = collision.position.x + collision.width;
+            if (prevTop >= collision.position.y + collision.height && top < collision.position.y + collision.height) this.position.y = collision.position.y + collision.height;
+            if (prevBottom >= collision.position.y + collision.height && bottom <= collision.position.y + collision.height) {
+                this.position.y = collision.position.y + collision.height;
+                this.velocity.y = 0;
+                this.canJump = true;
+            }
+        }
+
+
+        if (this.moving) {
+            if (this.update_timer.tick()) {
                 this.currentImageIndex += 1;
                 this.currentImageIndex %= this.images.length;
             }
-            
+
         }
         else {
             this.currentImageIndex = 0;
         }
 
         this.bullets.forEach((b) => b.update());
+        this.enemies.forEach((e) => e.update());
     }
+
+    get x() {
+        return this.position.x;
+    }
+    get y() {
+        return this.position.y;
+    }
+
+    set x(x) {
+        this.position.x = x;
+    }
+    set y(y) {
+        this.position.y = y;
+    }
+
+    reset() {
+        this.enemies = [];
+        this.bullets = [];
+        this.score = 0;
+        this.position.x = 50;
+        this.position.y = conf.GROUND_Y;
+        this.velocity.x = 0;
+        this.velocity.y = 0;
+    }
+
+    drawScore(ctx) {
+        ctx.fillStyle = "black";
+        ctx.fillText("Score: " + this.score, 20, 30)
+    }
+
 }
 
 export default Player;
